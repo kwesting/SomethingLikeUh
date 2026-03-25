b297f2670c12486988ef7e5fd0f5803e030882e7
kwesting <kwest107@wgu.edu>
2026-03-25 16:02:26 -0700

    cleanup basics files, new file

---
+++ datatype_basics.py
@@ -0,0 +1,7 @@
+n = 10
+if n < 50:
+    print("That number is TOO DAMN smol - less than 50")
+elif n == 50:
+    print("That's fuckin 50!!")
+else:
+    print("That is way too damn big friend")
--- devasc_python_basics.py
+++ devasc_python_basics.py
@@ -1,21 +1,22 @@
-favorite_cars = [
+favorite_cars =[
+
     {
-        "make": "BMW"
-        "model": "Mini Cooper"
+        "make": "BMW",
+        "model": "Mini Cooper",
         "year": 2012, #int
         "price": 999999.99, #float
         "features": ["Double sunroof", "fast", "cute", "noisy"], #list
-        "is_electric": False
-        "previous_owner": None
+        "is_electric": False,
+        "previous_owner": None,
     },
     {
-        "make": "Chevrolet"
-        "model": None
-        "year": 1955
+        "make": "Chevrolet",
+        "model": None,
+        "year": 1955,
         "price": 999.99, #float
         "features": ["big body", "fast", "cute", "noisy", "mauve"], #list
-        "is_electric": False
-        "previous_owner": Bob
+        "is_electric": False,
+        "previous_owner": "Bob",
 
     }
 ]
@@ -24,3 +25,6 @@ if favorite_cars == []:
     print("This list is empty.")
     print("Please add something for me to play with.")
     print("Dumbass")
+else:
+    print("This is a full af list bro")
+    print(favorite_cars[0]["make"] + " " + favorite_cars[0]["model"] )
